a636dd7eb671ce256a0982909c667a8185afeb6f
robertng0407 <robertng0407@gmail.com>
2018-11-20 16:31:49 -0800

    Import burger into burgerbuilder component

--- src/components/Burger/BurgerIngredients/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from './BurgerIngredients.css';
+import classes from './BurgerIngredient.css';
 
 
 class BurgerIngredient extends Component {
