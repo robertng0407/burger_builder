010312da514e6f35a8898b4ef3e26c6e24c76651
Robert Nguyen <robertng0407@gmail.com>
2018-11-20 19:00:58 -0800

    Add conditional that checks if ingredients are added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -13,7 +13,10 @@ const burger = (props) => {
             console.log(el)
             return arr.concat(el);
         }, []);
-        console.log(transformedIngredients)
+    console.log(transformedIngredients)
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
