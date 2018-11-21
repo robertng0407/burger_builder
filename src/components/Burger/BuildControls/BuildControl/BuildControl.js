5514e1b13ddb6f17191e7c65fe9444846c6771e8
Robert Nguyen <robertng0407@gmail.com>
2018-11-20 19:41:59 -0800

    Controls renders in buildcontrols component

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,8 @@ import classes from './BuildControl.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <Button className={classes.Less}>Less</Button>
-        <Button className={classes.More}>More</Button>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
     </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { classicNameResolver } from 'typescript';
+import BuildControl from './BuildControl/BuildControl';
 import classes from './BuildControls.css';
 
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
+
 const buildControls = (props) => (
-    <div className={classicNameResolver.BuildControls}>
-        
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} type={ctrl.type} />
+        ))}
     </div>
 );
 
