b264c9c5fb2be5c776322f797db0dae09a6afa68
robertng0407 <robertng0407@gmail.com>
2018-12-03 12:52:46 -0800

    Add function to toggle sidedrawer when menu button clicked

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Logo from '../../../Logo/Logo';
+
+const drawerToggle = (props) => (
+    <div onClick={props.clicked}>MENU</div>
+);
+
+export default drawerToggle;
