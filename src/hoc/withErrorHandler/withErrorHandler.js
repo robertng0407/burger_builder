b84396fe7811915bc854882b1d41c85de86c724d
robertng0407 <robertng0407@gmail.com>
2018-12-11 10:34:34 -0800

    Fix eject for response when component is destroyed

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -25,7 +25,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
         componentWillUnmount () {
             axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.request.eject(this.resInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
