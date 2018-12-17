6fea5bba1142956d260cce7b5e332958998af530
robertng0407 <robertng0407@gmail.com>
2018-12-17 11:19:31 -0800

    Navigate to checkout page when placing an order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,40 +101,42 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('Continue');
-        this.setState({
-            loading: true
-        });
+        // this.setState({
+        //     loading: true
+        // });
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Robert Nguyen',
-                address: {
-                    street: 'Test st',
-                    zipCode: '12345',
-                    country: 'USA',
-                    email: 'test@test.com'
-                }
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                console.log(response);
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                });
-            })
-            .catch(error => {
-                console.log(error);
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                });
-            });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Robert Nguyen',
+        //         address: {
+        //             street: 'Test st',
+        //             zipCode: '12345',
+        //             country: 'USA',
+        //             email: 'test@test.com'
+        //         }
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         console.log(response);
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         });
+        //     })
+        //     .catch(error => {
+        //         console.log(error);
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         });
+        //     });
+        this.props.history.push({
+            pathname: '/checkout'
+        })
     }
 
     render() {
