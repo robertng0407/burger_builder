9466c31cd5dc505d43f4be7a7bce32ff7895df0d
robertng0407 <robertng0407@gmail.com>
2018-12-18 17:25:08 -0800

    Add event handler for input elements with deep copy

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -91,6 +91,24 @@ class ContactData extends Component {
         console.log(this.props.ingredients)
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        this.setState({
+            orderForm: updatedOrderForm
+        });
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -102,11 +120,12 @@ class ContactData extends Component {
         let form = (
             <form>
                 {formElementsArray.map(formElement => (
-                    <Input 
-                        key={formElement.id} 
-                        elementType={formElement.config.elementType} 
-                        elementConfig={formElement.config.elementConfig} 
-                        value={formElement.config.value} />
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
                 <Button
                     btnType="Success"
