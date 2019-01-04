d9c5508287b462a02f523b742c275535577f5fe5
robertng0407 <robertng0407@gmail.com>
2019-01-04 15:53:10 -0800

    Add authentication route with validations

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
       <div>
         <Layout>
           <Switch>
+            <Route path="/auth" component={Auth} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
