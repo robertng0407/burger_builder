6b0122b3dcedea6424236fd96109c340dc51c1b2
robertng0407 <robertng0407@gmail.com>
2019-01-28 15:28:30 -0800

    Check to see if logout button exists if user is logged in

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -24,7 +24,13 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({
             isAuth: true
         });
-
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render three <NavigationItems /> elements if authenticated', () => {
+        wrapper.setProps({
+            isAuth: true
+        });
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
